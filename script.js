589005a5eda58d7bedbe28a7a5a634842b85eb88
Human757 <131761012+unnelzi@users.noreply.github.com>
2024-09-03 13:07:19 +0300

    finally fixing the bug

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ function incrementTime(unit, amount) {
   if (!isRunning) {
     switch (unit) {
       case "hours":
-        if (remainingTime + amount <= 356400) {
+        if (remainingTime + amount < 359999) {
           remainingTime += amount * 3600;
         }
         break;
